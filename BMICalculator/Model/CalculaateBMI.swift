a7606ee98ba9686faaae98f49f87085bdfdf6690
Ahmed <ahmeddhalilovic@gmail.com>
2024-09-25 22:38:54 +0200

    small changes

--- BMICalculator/Model/CalculaateBMI.swift
+++ BMICalculator/Model/CalculaateBMI.swift
@@ -22,7 +22,7 @@ struct CalculateBMI {
         } else if bmiVal <= 29.9 {
             bmi = BMI(value: bmiVal, adviceText: "Eat less and control yourself", color: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
         } else {
-            bmi = BMI(value: bmiVal, adviceText: "Hit the Gym", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+            bmi = BMI(value: bmiVal, adviceText: "Hit the Gym and do not eat at all", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
         }
         
     }
